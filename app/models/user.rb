4bae3e2a02fc5f9379b7aa81b936fe60531a7b56
juancarlosasensio <jasensio@ufm.edu>
2020-02-24 15:00:27 +0000

    Add devise User model in ApplicationController

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :lockable, :trackable
 end
