24c2f3716b9bf45aac34b611f8348ba4b7fc5d95
Dominic Ferris <ferris.dominic@gmail.com>
2020-02-24 17:15:01 +0000

    init user show page, modify new user form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get '/profile', to: 'users#show'
+
   resources :studios, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
