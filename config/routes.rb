2074fc1e7799fd42e15425bea33671755e9b4947
Dominic Ferris <ferris.dominic@gmail.com>
2020-02-24 16:31:23 +0000

    studio model, controller, routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+
+  devise_for :users
+
+  resources :studios, only: [:new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
