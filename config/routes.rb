8cea32be974a8585ca153db513746b7aed191c33
Jack Holdsworth <jackch90@gmail.com>
2020-02-25 12:53:36 +0000

    Model and controller created, working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   get '/profile', to: 'users#show'
 
-  resources :studios, only: [:new, :create]
+  resources :studios, only: [:new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
