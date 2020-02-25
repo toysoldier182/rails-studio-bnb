c44906b96b05854b960e824e002589bafdde607a
Jack Holdsworth <jackch90@gmail.com>
2020-02-25 10:35:18 +0000

    Created seed for user and studio.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    email: Faker::Internet.email,
+    password: '********'
+    )
+end
+
+50.times do
+  Studio.create(
+    name: Faker::Address.street_name,
+    location: Faker::Address.street_address,
+    price: rand(1..100),
+    user_id: rand(1..50),
+    )
+end
