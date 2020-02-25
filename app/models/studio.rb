fb6e020b55e0ceb7bf1fd9be2bc39db23c20acce
juancarlosasensio <jasensio@ufm.edu>
2020-02-25 17:16:49 +0000

    Create new booking form

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,5 +1,6 @@
 class Studio < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   # has_many_attached :photos
 
   validates :name, :location, :price, presence: true
