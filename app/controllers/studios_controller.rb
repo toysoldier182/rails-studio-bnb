150f495d4f2acaab2d6b3fe6ef0c8106d024cf45
juancarlosasensio <jasensio@ufm.edu>
2020-02-25 17:35:30 +0000

    Removed user authentication from Studio index and show

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,5 +1,6 @@
 class StudiosController < ApplicationController
   before_action :find_studio, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @studios = Studio.all
