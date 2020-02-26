286cda119ad71cf541dc703ad452686eda1416d5
juancarlosasensio <jasensio@ufm.edu>
2020-02-26 14:27:37 +0000

    marco push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  has_many :studios
-  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :omniauthable, :lockable,
   # :trackable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :studios
+  has_one_attached :avatar
 
   validates :first_name, :last_name, presence: true
 end
