c9864c57607798935b9727bf8bdca778a1c8ed79
Jack Holdsworth <jackch90@gmail.com>
2020-02-27 13:52:17 +0000

    Minor edits to navbar (unfinished) and the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @studios = Studio.first(3)
+    @studios = Studio.first(6)
   end
 end
