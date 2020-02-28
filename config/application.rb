b8f1b826955e5d0dcbb0d6549c71c96b1d4a2b73
Jack Holdsworth <jackch90@gmail.com>
2020-02-28 11:58:05 +0000

    Changes to Search - debugging ajax

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module RailsStudioBnb
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
